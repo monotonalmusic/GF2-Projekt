d4758cfc315674409dd65ca5d53cb3b99d251e0f
Gabriel <gehughes28@gmail.com>
2022-12-21 15:51:48 +0100

    shortened generateNumbers function, will shorten maxFive and sort multiarray so that smallest number appears above next session

--- Bango/script.js
+++ Bango/script.js
@@ -40,94 +40,56 @@ function threeTimes() {
     generateNumbers(seed, 3)
 } 
 
-//Generer 15 tal, og kalder maxFive 
+function lengthOfMultiArray(array, biggerArrayLength) {
+    let i = 0;
+    biggerArrayLength = 0;
+    while (i<9) {
+        biggerArrayLength += array[i].length;
+        i++;
+    }
+    return biggerArrayLength;
+}
+
+// function sortMultiArray(array) {
+//     for(let i = 0; i<10; i++) {
+//     array.sort(function(a, b) {return a - b});
+//     }
+//     return array
+// }
+
+
 function generateNumbers(seed, container) {
-    let arrayOfBingoNumbers = [];
-    let firstColumnAmount = 0;
-    let secondColumnAmount = 3;
-    let thirdColumnAmount = 6;
-    let fourthColumnAmount = 9;
-    let fifthColumnAmount = 12;
-    let sixthColumnAmount = 15;
-    let seventhColumnAmount = 18;
-    let eighthColumnAmount = 21;
-    let ninthColumnAmount = 24;
-    loop:
-    for(let i = 0; arrayOfBingoNumbers.length < 15; i++) {
+    let arrayOfBingoNumbers = [[],[],[],[],[],[],[],[],[]];
+    let biggerArrayLength = 0;
+    for(let i = 0; biggerArrayLength < 15; i++) {
         let bingoNumber = new Math.seedrandom(seed + i + "x" + container*97);
         let bingoNumber2 = bingoNumber() * 90;
         bingoNumber2 = Math.round(bingoNumber2);
-        if(arrayOfBingoNumbers.indexOf(bingoNumber2) === -1 && 1 <= bingoNumber2 && bingoNumber2 <= 90) {
-            
-            
-            if(bingoNumber2 >= 1 && bingoNumber2 <= 9 && firstColumnAmount < 3) {
-                arrayOfBingoNumbers.push(bingoNumber2);
-                firstColumnAmount++;
+                for(let j = 0, k=10, l=0; l < 8; j+=10, k+=10, l++) {
+                    if(arrayOfBingoNumbers.indexOf(bingoNumber2) === -1 && 1 <= bingoNumber2 && bingoNumber2 <= 90) {
+                        if(bingoNumber2 >= j && bingoNumber2 < k && arrayOfBingoNumbers[l].length < 3 && arrayOfBingoNumbers[l][0] != bingoNumber2 && arrayOfBingoNumbers[l][1] != bingoNumber2) {
+                            arrayOfBingoNumbers[l].push(bingoNumber2);
+                        }
+
+                        else if(bingoNumber2 >= 80 && bingoNumber2 <= 90 && arrayOfBingoNumbers[8].length < 3 && arrayOfBingoNumbers[8][0] != bingoNumber2 && arrayOfBingoNumbers[8][1] != bingoNumber2) {
+                            arrayOfBingoNumbers[8].push(bingoNumber2);   
+                        } 
+                          
+                }
             }
-            else if (bingoNumber2 >= 10 && bingoNumber2 <= 19 && secondColumnAmount < 6) {
-                arrayOfBingoNumbers.push(bingoNumber2);
-                secondColumnAmount++;
-            }
-            else if (bingoNumber2 >= 20 && bingoNumber2 <= 29 && thirdColumnAmount < 9) {
-                arrayOfBingoNumbers.push(bingoNumber2);
-                thirdColumnAmount++;
-            }
-            else if (bingoNumber2 >= 30 && bingoNumber2 <= 39 && fourthColumnAmount < 12) {
-                arrayOfBingoNumbers.push(bingoNumber2);
-                fourthColumnAmount++;
-            }
-            else if (bingoNumber2 >= 40 && bingoNumber2 <= 49 && fifthColumnAmount < 15) {
-                arrayOfBingoNumbers.push(bingoNumber2);
-                fifthColumnAmount++;
-            }
-            else if (bingoNumber2 >= 50 && bingoNumber2 <= 59 && sixthColumnAmount < 18) {
-                arrayOfBingoNumbers.push(bingoNumber2);
-                sixthColumnAmount++;
-            }
-            else if (bingoNumber2 >= 60 && bingoNumber2 <= 69 && seventhColumnAmount < 21) {
-                arrayOfBingoNumbers.push(bingoNumber2);
-                seventhColumnAmount++;
-            }
-            else if (bingoNumber2 >= 70 && bingoNumber2 <= 79 && eighthColumnAmount < 24) {
-                arrayOfBingoNumbers.push(bingoNumber2);
-                eighthColumnAmount++;
-            }
-            else if (bingoNumber2 >= 80 && bingoNumber2 <= 90 && ninthColumnAmount < 27) {
-                arrayOfBingoNumbers.push(bingoNumber2);
-                ninthColumnAmount++;
-            }         
-        }
-       
-        
-    } 
-    arrayOfBingoNumbers.sort(function(a, b) {return a-b});
-    console.log(arrayOfBingoNumbers);
+        biggerArrayLength = lengthOfMultiArray(arrayOfBingoNumbers, biggerArrayLength);
+    }
+    sortMultiArray(arrayOfBingoNumbers);
     maxFive(arrayOfBingoNumbers, container);
-
-
 }
-
-//filters array to find values between given lower value and upper value
- function findNumberInArray(lower, upper, array) {
-     let between = array.filter(function(item) {
-         return (item >= lower && item <= upper);
-     })
-     return between
- }
 
 
 //begraenser antal tal i row til max 5
 function maxFive(array, container) {
-    let emptyArray = [];
-    for(let i = 0, lower = 0, upper = 9; i<8; i++, lower +=10, upper+=10) {
-        emptyArray.push(findNumberInArray(lower, upper, array));
-    }
-    emptyArray.push(findNumberInArray(80, 90, array));
-    emptyArray.sort(function(a, b) {return b.length - a.length});
-    console.log(emptyArray);
+    array.sort(function(a, b) {return b.length - a.length});
     let rowNum = 2;
         for(let j = 0; j<=4; j++) {
-            let lastItem = emptyArray[j].pop();
+            let lastItem = array[j].pop();
             
             for(let k = 10, l = 0, m = 0; m<8; k+=10, l+=10, m++) {  
                 if(lastItem < k && lastItem >= l) {
@@ -142,13 +104,13 @@ function maxFive(array, container) {
                 }
                 
             }
-            console.log(emptyArray)
+            
             
         }
-        emptyArray.sort(function(a, b) {return b.length - a.length});
+        array.sort(function(a, b) {return b.length - a.length});
         rowNum = 1;
         for(let j = 0; j<=4; j++) {
-            let lastItem = emptyArray[j].pop();
+            let lastItem = array[j].pop();
             
             for(let k = 10, l = 0, m = 0; m<8; k+=10, l+=10, m++) {  
                 if(lastItem < k && lastItem >= l) {
@@ -163,13 +125,13 @@ function maxFive(array, container) {
                 }
                 
             }
-            console.log(emptyArray)
+            
             
         }
-        emptyArray.sort(function(a, b) {return b.length - a.length});
+        array.sort(function(a, b) {return b.length - a.length});
         rowNum = 0;
         for(let j = 0; j<=4; j++) {
-            let lastItem = emptyArray[j].pop();
+            let lastItem = array[j].pop();
             
             for(let k = 10, l = 0, m = 0; m<8; k+=10, l+=10, m++) {  
                 if(lastItem < k && lastItem >= l) {
@@ -184,7 +146,7 @@ function maxFive(array, container) {
                 }
                 
             }
-            console.log(emptyArray)
+            
         }
     
 }
